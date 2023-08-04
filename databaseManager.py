ffc68c360cdaf6ec499b7d053a6039c9f4576ae7
James Muguiyi <jmuguiyijr@gmail.com>
2023-08-04 13:33:11 -0400

    Initial commit

---
+++ databaseManager.py
@@ -0,0 +1,46 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': ""
+})
+
+ref = db.reference('Students')
+
+data = {
+    "732710":
+        {
+            "name": "James Muguiyi",
+            "major": "Comp Sci",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2023-06-11 00:54:34"
+        },
+    "374901":
+        {
+            "name": "Emily Blunt",
+            "major": "Economics",
+            "starting_year": 2021,
+            "total_attendance": 12,
+            "standing": "B",
+            "year": 1,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "590121":
+        {
+            "name": "Elon Musk",
+            "major": "Physics",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        }
+}
+
+for key, value in data.items():
+    ref.child(key).set(value)
